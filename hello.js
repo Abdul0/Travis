2261245240c4534e20644b2bc2a1043363ea68cb
Abdullah <a.a.mg@mail.net.sa>
2016-03-17 11:44:45 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,15 +1,6 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello Abdullah Alqarny"
-   response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Abdu!\n') // this will FAIL travis ci lint
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
